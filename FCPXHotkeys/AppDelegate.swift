ca8d756489a37bd331343727c7e5f7bb8b0d9e0b
Tim Nelson <tdnelson@gmail.com>
2017-03-14 15:25:09 -0700

    Initial Commit

---
+++ FCPXHotkeys/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FCPXHotkeys
+//
+//  Created by Editor on 3/14/17.
+//  Copyright © 2017 Editor. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FCPXHotkeys/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FCPXHotkeys
+//
+//  Created by Editor on 3/14/17.
+//  Copyright © 2017 Editor. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
